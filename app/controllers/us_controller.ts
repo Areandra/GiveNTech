7fa5de29175e79b27722ba3a940fe423bd560a52
NanCyfeuy <adnanmuftimaulana0@gmail.com>
2025-10-20 04:01:48 +0800

    update

--- app/controllers/us_controller.ts
+++ app/controllers/us_controller.ts
@@ -1,14 +1,14 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UserController {
-  async index({ auth, inertia }: HttpContext) {
-    const user = auth.user!
-    await user.load('bookings', (query) => {
-      query.select(['id', 'no_ruang', 'nama'])
-    })
+    async index({ auth, inertia }: HttpContext) {
+        const user = auth.user!
+        await user.load('bookings', (query) => {
+            query.select(['id', 'no_ruang', 'nama'])
+        })
 
-    return inertia.render('user/dashboard', {
-      user,
-    })
-  }
+        return inertia.render('user/dashboard', {
+            user,
+        })
+    }
 }
